d6d0669a92aa5600e9feff805f2d98265518abc2
Rafael Alfaro <rafaela@thehangar.cr>
2018-07-27 08:12:55 -0600

    fixes activity_log migration

--- database/migrations/2018_07_26_195752_create_activity_log_table.php
+++ database/migrations/2018_07_26_195752_create_activity_log_table.php
@@ -3,27 +3,29 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateActivityLogTable extends Migration
-{
+class CreateActivityLogTable extends Migration {
     /**
      * Run the migrations.
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('activity_log', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->nullable();
-            $table->string('text');
-            $table->string('ip_address', 64);
+            $table->string('log_name')->nullable();
+            $table->text('description');
+            $table->integer('subject_id')->nullable();
+            $table->string('subject_type')->nullable();
+            $table->integer('causer_id')->nullable();
+            $table->string('causer_type')->nullable();
+            $table->text('properties')->nullable();
             $table->timestamps();
+            $table->index('log_name');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down()
-    {
+    public function down() {
         Schema::drop('activity_log');
     }
 }
